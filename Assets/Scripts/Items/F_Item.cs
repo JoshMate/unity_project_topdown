1e1a4e14ffd4b6508557ea1845ef9fe0b62327ce
Josh Atkins <joshwar99@gmail.com>
2025-04-21 20:03:21 +0100

    Item Class creation and basic item objects

---
+++ Assets/Scripts/Items/F_Item.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class F_Item : MonoBehaviour
+{
+    [Header("Item Information")]
+
+    public string itemName = "<Item Name>";
+    public string itemDescription = "<Item Name>";
+    public float itemWeight = 0.0f;
+    public int itemValue = 1;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
